34052c926557e31ebc6637de014947306d466518
kamszef <kszefler.kontakt@gmail.com>
2023-10-06 17:37:53 +0200

    Basic Id data generation

--- main.py
+++ main.py
@@ -24,13 +24,28 @@ def sheetGenerator(elementarCell, x, y):
     This function generates pentagraphene sheet.
     """
     sheet = []
-    for node in elementarCell:
-        for i in range(x):
-            for j in range(y):
-                xCoordinate = node[0] + i
-                yCoordinate = node[1] + j
-                zCoordinate = node[2]
-                sheet.append([xCoordinate, yCoordinate, zCoordinate])
+    unitCellId = 1
+
+    for i in range(y):
+            for j in range(x):
+                atoms = []
+
+                for k in range(6):
+                    atoms.append({
+                        "id_global": elementarCell[k][3] + (unitCellId - 1) * 6,
+                        "id_local": elementarCell[k][3],
+                        "id_local": elementarCell[k][3],
+                        "coordinates": [elementarCell[k][0] + 1 * j , elementarCell[k][1] + 1 * i, elementarCell[k][2]]
+                    })
+
+                sheet.append({
+                     "id_cell": unitCellId,
+                      "m": j,
+                      "n": i,
+                      "atoms": atoms
+                })
+
+                unitCellId += 1
 
     return sheet
 
@@ -49,8 +64,10 @@ y = int(input("Enter repetitions on y axis: "))
 elementarCell = calcElementarCell(d22, h)
 sheet = sheetGenerator(elementarCell, x, y)
 
-with open("pentagraphene.xyz", "w") as f:
-    f.write(str(len(sheet)) + "\n")
-    f.write("Sheet\n")
-    for node in sheet:
-        f.write("C " + str(node[0]) + " " + str(node[1]) + " " + str(node[2]) + "\n")
+print(sheet)
+
+# with open("pentagraphene.xyz", "w") as f:
+#     f.write(str(len(sheet)) + "\n")
+#     f.write("Sheet\n")
+#     for node in sheet:
+#         f.write("C " + str(node[0]) + " " + str(node[1]) + " " + str(node[2]) + "\n")
